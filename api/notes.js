a1b639d51d3d84b9d0f656b00f5730e9152e5426
cosmicsys <samuelsimoni850@gmail.com>
2026-05-02 08:47:01 +0200

    Update notes.js

--- api/notes.js
+++ api/notes.js
@@ -1,12 +1,15 @@
-let notes = []
+import { kv } from "@vercel/kv"
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method === "POST") {
+        const notes = await kv.get("notes") || []
         notes.push(req.body)
+        await kv.set("notes", notes)
         res.status(200).end()
     } else if (req.method === "GET") {
+        const notes = await kv.get("notes") || []
         res.status(200).json(notes)
     } else {
         res.status(405).end()
     }
-} 
+}
